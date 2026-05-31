c218263b8acd8080392ff3912c250d28dd457a5e
p-sw <shinwoo.park@psw.kr>
2026-05-31 23:43:12 +0900

    feat: add dbPath to config

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,14 @@ import "dotenv/config";
 
 export interface Config {
   openrouterApiKey: string;
+  dbPath: string;
 }
 
 const openrouterApiKey = process.env["OPENROUTER_API_KEY"];
 if (!openrouterApiKey) throw new Error("OPENROUTER_API_KEY is missing");
+const dbPath = process.env["DB_PATH"] ?? "sqlite.db";
 
 export const config: Config = {
-  openrouterApiKey: openrouterApiKey,
+  openrouterApiKey,
+  dbPath,
 };
